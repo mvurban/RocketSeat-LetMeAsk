d6e93850e484e30f391d31689ff2b855d3f4ce42
Mvurban <marcelo.vendas@iprj.uerj.br>
2022-03-14 12:07:49 -0300

    firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {database, auth} from './services/firebase';
 
 ReactDOM.render(
   <React.StrictMode>
