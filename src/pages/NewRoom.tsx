56a078ba3030f8743ebc48729bf61b81ebec69be
Mvurban <marcelo.vendas@iprj.uerj.br>
2022-03-23 10:55:07 -0300

    Tela NewRoom

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,5 +1,36 @@
+import ilustration from '../../src/pages/assets/images/ilustration.svg'
+import logoImg from '../../src/pages/assets/images/logo.svg'
+import googleIconImg from '../../src/pages/assets/images/google_icon.svg'
+import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
+
+
 export function NewRoom(){
-   return(
-      <></>
-   );
+    return (
+        <div id='page-auth'>
+
+            <aside>
+                <img src={ilustration} alt="Ilustração da Home"></img>
+                <strong>Toda pergunta tem uma resposta.</strong>
+                <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+            </aside>
+           
+            <main>
+                <div className='main-content'>  
+                    <img src={logoImg} alt="logo"></img>
+                    <h2>Crie uma nova sala</h2>                    
+                    <form>
+                        <input type='text' placeholder='nome da sala'></input>
+                        <Button  type='submit'>Criar sala</Button>
+                    </form>
+                    <p>
+                        Quer entrar numa sala já existente?
+                        <a href=''>clique aqui</a>
+                     </p>
+                </div>
+            </main> 
+
+        </div>
+    );
 }
