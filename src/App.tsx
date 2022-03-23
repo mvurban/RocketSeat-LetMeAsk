718b93497129924be0357a8f919a33d1142bbf7e
Marcelo Vendas <mvurban@mvurban.eti.br>
2022-03-23 20:02:49 -0300

    ajustes bobos

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ import {BrowserRouter, Route, Routes  } from 'react-router-dom';
 
 function App() {
    return (
+      
+      
       <BrowserRouter>
          <Routes>
-            <Route path="/" element={<Home />} ></Route> 
+            <Route path="/"  element={<Home />} ></Route> 
             <Route path="/Room/New" element={<NewRoom/>}></Route>     
          </Routes>
       </BrowserRouter>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,6 +16,7 @@ export function Home(){
 
       try{
          const credential = await googleLoginWithPopUp();
+         navigate("/Room/New")
          //console.log(credential.user);
       }
       catch(error){
