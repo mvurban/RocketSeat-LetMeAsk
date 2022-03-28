cb488c3cc0d647ee36c53804f6c41e9e6cfe0db4
Mvurban <marcelo.vendas@iprj.uerj.br>
2022-03-28 15:03:09 -0300

    iniciando o desenvolvimento de uma nova sala.

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,7 @@
+
+
+export function RoomCode(){
+   return(
+      <button></button>
+   );
+}
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Button } from "../components/Button";
 import { useRoom } from "../domain/Room";
+import '../styles/room.scss';
+import logo from './assets/images/logo.svg'
 
 export function Room(){
 
@@ -12,8 +15,8 @@ export function Room(){
       try {
          setNameToRoom(idRoom);
       } 
-      catch (e) {
-         alert(e)
+      catch (error) {
+         alert(error)
       }      
    }
    
@@ -26,13 +29,47 @@ export function Room(){
          if (room){
             setRoomName(room?.title)      
          }
-      } catch (e) {
-         alert(e)
+      } catch (error) {
+         alert(error)
       }
    }
 
+   function handleSendQuestion(event:FormEvent) {
+      event.preventDefault();
+   }
 
    return(
-      <h1>Nome da sala: {roomName}</h1>
+
+      <div className="page-container">
+         <header>
+            <img src={logo} alt="logo"></img>
+            <span>Sala {idRoom}</span>
+         </header>
+         <main>
+            <div className="room-title">
+               <h1>{roomName}</h1>
+               <span>4 perguntas</span>
+            </div>
+            <form onSubmit={handleSendQuestion}>
+               <textarea
+                  placeholder="O que você quer perguntar?"
+               >
+               </textarea>
+               <div className="form-footer">
+                  <span>Para enviar uma pergunta, <button type="submit">faça seu login.</button></span>
+                  <Button type="submit">Enviar pergunta</Button>
+               </div>
+            </form>
+
+            <section>
+               <span>Lista de perguntas</span>
+            </section>
+            
+
+         </main>
+
+      </div>
+
+
    );
 }
